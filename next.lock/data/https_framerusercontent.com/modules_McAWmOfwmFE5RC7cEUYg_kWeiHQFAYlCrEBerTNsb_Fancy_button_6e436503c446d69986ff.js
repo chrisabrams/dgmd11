6c6f6880341fe116aa7ee052ff812085926b9202
Chris Abrams <mail@chrisabrams.com>
2022-01-31 17:12:42 -0600

    add framer component

---
+++ next.lock/data/https_framerusercontent.com/modules_McAWmOfwmFE5RC7cEUYg_kWeiHQFAYlCrEBerTNsb_Fancy_button_6e436503c446d69986ff.js
@@ -0,0 +1,11 @@
+import{jsx as _jsx}from"react/jsx-runtime";import{useState}from"react";import{motion}from"framer-motion";import{addPropertyControls,ControlType}from"framer";// Welcome to Code in Framer
+// Get Started: https://www.framer.com/docs/guides/
+/**
+ * These annotations control how your component sizes
+ * Learn more: https://www.framer.com/docs/guides/auto-sizing
+ *
+ * @framerSupportedLayoutWidth any
+ * @framerSupportedLayoutHeight any
+ */ export default function Fancy_button(props){const{tint,style}=props;const[active,setActive]=useState(false);return(/*#__PURE__*/ _jsx(motion.div,{style:{...style,...containerStyle},children:/*#__PURE__*/ _jsx(motion.div,{onTap:()=>setActive(!active),style:{margin:50,padding:75,borderRadius:30,backgroundColor:tint},whileHover:{rotate:90},animate:{scale:active?1.25:1}})}));};Fancy_button.defaultProps={tint:"#09F"};addPropertyControls(Fancy_button,{tint:{title:"Tint",type:ControlType.Color}});const containerStyle={display:"flex",justifyContent:"center",alignItems:"center",overflow:"hidden"};
+export const __FramerMetadata__ = {"exports":{"default":{"type":"reactComponent","name":"Fancy_button","slots":[],"annotations":{"framerContractVersion":"1","framerSupportedLayoutWidth":"any","framerSupportedLayoutHeight":"any"}},"__FramerMetadata__":{"type":"variable"}}}
+//# sourceMappingURL=./Fancy_button.map
